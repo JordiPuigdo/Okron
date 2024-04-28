fbe69f2e76bb937657c5aa621c086889a54b7be9
JordiPuigdollers <jordi.puigdollers@holaglow.com>
2024-04-28 22:47:13 +0200

    commit changes

--- src/app/(pages)/menu/dasboard/page.tsx
+++ src/app/(pages)/menu/dasboard/page.tsx
@@ -1,13 +1,106 @@
 "use client";
+import { UserPermission } from "app/interfaces/User";
+import {
+  SearchWorkOrderFilters,
+  WorkOrderType,
+} from "app/interfaces/workOrder";
+import OperatorService from "app/services/operatorService";
+import WorkOrderService from "app/services/workOrderService";
+import { useSessionStore } from "app/stores/globalStore";
 import { BarChartComponent } from "designSystem/BarChart/BarChartComponent";
 import { DonutChartComponent } from "designSystem/DonutChart/DonutChartComponent";
+import { useEffect, useState } from "react";
+
+interface WorkOrdersChartProps {
+  operator: string;
+  numberPreventive: number;
+  numberCorrective: number;
+}
 
 export default function DasboardPage() {
+  const workOrderService = new WorkOrderService(
+    process.env.NEXT_PUBLIC_API_BASE_URL!
+  );
+  const operatorService = new OperatorService(
+    process.env.NEXT_PUBLIC_API_BASE_URL!
+  );
+  const currentDate = new Date();
+  const firstDayOfMonth = new Date(
+    currentDate.getFullYear(),
+    currentDate.getMonth(),
+    1
+  );
+
+  const { loginUser } = useSessionStore((state) => state);
+
+  const [chartData, setChartData] = useState<any[]>([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const operators = await operatorService.getOperators();
+
+      const filters: SearchWorkOrderFilters = {
+        assetId: "",
+        operatorId: "",
+        startDateTime: firstDayOfMonth.toISOString(),
+        endDateTime: currentDate.toISOString(),
+      };
+      await workOrderService
+        .getWorkOrdersWithFilters(filters)
+        .then((response) => {
+          if (!operators) return;
+          const operatorMap = new Map<string, WorkOrdersChartProps>();
+
+          response.forEach((workOrder) => {
+            const operatorId = workOrder.operatorId?.map((op) => op) || [];
+
+            operatorId.forEach((operatorName) => {
+              const existingOperator = operatorMap.get(operatorName);
+
+              if (existingOperator) {
+                if (workOrder.workOrderType === WorkOrderType.Preventive) {
+                  existingOperator.numberPreventive++;
+                } else if (
+                  workOrder.workOrderType === WorkOrderType.Corrective
+                ) {
+                  existingOperator.numberCorrective++;
+                }
+              } else {
+                const newOperatorEntry: WorkOrdersChartProps = {
+                  operator:
+                    operators.find((x) => x.id === operatorName)?.name ||
+                    "Proves",
+                  numberPreventive:
+                    workOrder.workOrderType === WorkOrderType.Preventive
+                      ? 1
+                      : 0,
+                  numberCorrective:
+                    workOrder.workOrderType === WorkOrderType.Corrective
+                      ? 1
+                      : 0,
+                };
+                operatorMap.set(operatorName, newOperatorEntry);
+              }
+            });
+          });
+
+          const data = Array.from(operatorMap.values());
+          setChartData(data);
+        });
+    }
+    if (loginUser?.permission == UserPermission.Administrator) fetchData();
+  }, []);
+  if (loginUser?.permission !== UserPermission.Administrator) return <></>;
   return (
     <>
       <div className="flex flex-row gap-12 w-full items-center bg-white p-4 rounded-xl">
-        <BarChartComponent />
-        <DonutChartComponent />
+        {chartData.length > 0 && (
+          <BarChartComponent
+            category={["numberPreventive", "numberCorrective"]}
+            chartData={chartData}
+            index="operator"
+          />
+        )}
       </div>
     </>
   );
--- src/designSystem/BarChart/BarChartComponent.tsx
+++ src/designSystem/BarChart/BarChartComponent.tsx
@@ -1,40 +1,27 @@
 import { BarChart } from "@tremor/react";
 
-const chartdata = [
-  {
-    name: "Xevi",
-    Preventius: 2,
-    Correctius: 15,
-  },
-  {
-    name: "Jose",
-    Preventius: 2,
-    Correctius: 3,
-  },
-  {
-    name: "EMERSON",
-    Preventius: 0,
-    Correctius: 3,
-  },
-  {
-    name: "AMIR",
-    Preventius: 0,
-    Correctius: 2,
-  },
-];
+interface BarChartComponentProps {
+  chartData: any[];
+  index: string;
+  category: string[];
+}
 
 const dataFormatter = (number: number) =>
   Intl.NumberFormat("us").format(number).toString();
 
-export function BarChartComponent() {
+export function BarChartComponent({
+  chartData,
+  index,
+  category,
+}: BarChartComponentProps) {
   return (
     <>
       <h3 className="text-lg font-medium">Ordres de treball del mes</h3>
       <BarChart
         className="mt-6"
-        data={chartdata}
-        index="name"
-        categories={["Preventius", "Correctius"]}
+        data={chartData}
+        index={index}
+        categories={category}
         colors={["blue", "teal", "amber", "rose", "indigo", "emerald"]}
         valueFormatter={dataFormatter}
         yAxisWidth={48}
