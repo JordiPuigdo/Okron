19d4e6936a1f67902c991a49285c2827ebbe4912
JordiHolaGlow <jordi.puigdollers@holaglow.com>
2024-10-24 10:02:12 +0200

    commit

--- src/app/(pages)/reports/spareParts/consumedSpareParts/consumedSparePartsComponent.tsx
+++ src/app/(pages)/reports/spareParts/consumedSpareParts/consumedSparePartsComponent.tsx
@@ -10,13 +10,15 @@ import SparePartsConsumedReportTable from "./component/SparePartsConsumedReportT
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import ca from "date-fns/locale/ca";
-import { SvgSpinner } from "app/icons/icons";
+import { SvgArrowRight, SvgSpinner } from "app/icons/icons";
 import SparePartBarchart from "./component/sparePartBarchart";
+import { useRouter } from "next/navigation";
 
 dayjs.extend(utc);
 dayjs.extend(timezone);
 
 export default function ConsumedSparePartsComponent() {
+  const router = useRouter();
   const [from, setFrom] = useState(() => {
     const date = new Date();
     date.setDate(date.getDate() - 7);
@@ -32,25 +34,48 @@ export default function ConsumedSparePartsComponent() {
 
   return (
     <div>
-      <div className="flex gap-4 bg-white rounded-xl p-2 shadow-md">
-        <DatePicker
-          selected={from}
-          onChange={(e) => (e ? setFrom(e) : setFrom(new Date()))}
-          locale={ca}
-          dateFormat="dd/MM/yyyy"
-          className="border border-gray-300 p-2 rounded-md mr-4 w-full"
-        />
-        <DatePicker
-          selected={to}
-          onChange={(e) => (e ? setTo(e) : setTo(new Date()))}
-          dateFormat="dd/MM/yyyy"
-          locale={ca}
-          className="border border-gray-300 p-2 rounded-md mr-4 w-full"
-        />
+      <div className="flex flex-col gap-4 bg-white rounded-xl p-4 shadow-md">
+        <div className="w-full flex flex-row text-xl font-semibold p-2 items-center border-b-2 border-gray-300">
+          <div className="flex cursor-pointer " onClick={() => router.back()}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+              className="w-6 h-6 inline-block mr-2"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M10 19l-7-7m0 0l7-7m-7 7h18"
+              />
+            </svg>
+          </div>
+          <div className="flex w-full text-center justify-center">
+            Històric de consums de recanvis
+          </div>
+        </div>
+        <div className="flex gap-4">
+          <DatePicker
+            selected={from}
+            onChange={(e) => (e ? setFrom(e) : setFrom(new Date()))}
+            locale={ca}
+            dateFormat="dd/MM/yyyy"
+            className="border border-gray-300 p-2 rounded-md mr-4 w-full"
+          />
+          <DatePicker
+            selected={to}
+            onChange={(e) => (e ? setTo(e) : setTo(new Date()))}
+            dateFormat="dd/MM/yyyy"
+            locale={ca}
+            className="border border-gray-300 p-2 rounded-md mr-4 w-full"
+          />
 
-        <Button type="create" onClick={() => reloadSparePartsConsumeds()}>
-          Buscar
-        </Button>
+          <Button type="create" onClick={() => reloadSparePartsConsumeds()}>
+            Buscar
+          </Button>
+        </div>
       </div>
       {isLoading && (
         <div className="flex w-full text-white justify-center p-4">
